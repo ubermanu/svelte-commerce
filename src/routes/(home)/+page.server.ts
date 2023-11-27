897bc93f5071cd8e049eac3c9c1e251a0a6f1875
ubermanu <manu@paca.happy-dev.fr>
2023-11-27 18:33:18 +0100

    load home cms page in page server

--- src/routes/(home)/+page.server.ts
+++ src/routes/(home)/+page.server.ts
@@ -1,10 +1,9 @@
 import { getCmsPage } from '$lib/server/magento'
 import type { ServerLoad } from '@sveltejs/kit'
 
-export const load: ServerLoad = async ({ locals }) => {
+export const load: ServerLoad = async () => {
   const cmsPage = await getCmsPage('home')
   return {
-    customer: locals.customer,
     cmsPage,
   }
 }
