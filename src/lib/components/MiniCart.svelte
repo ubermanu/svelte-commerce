dbad216330a3ba26ac507c634cdec69e8dea625a
ubermanu <manu@paca.happy-dev.fr>
2023-11-27 20:48:41 +0100

    feat: basic minicart

--- src/lib/components/MiniCart.svelte
+++ src/lib/components/MiniCart.svelte
@@ -1,8 +1,73 @@
-<script>
-  export let cart
-  console.log('cart', cart)
+<script lang="ts">
+  import PriceBox from '$lib/components/PriceBox.svelte'
+  import clickOutside from '$lib/actions/clickOutside'
+  import { writable } from 'svelte/store'
+
+  export let cart: any
+  console.log(cart)
+
+  // Create a store to keep track of the visibility of the mini cart
+  const visible = writable(false)
+
+  function toggle() {
+    $visible = !$visible
+  }
 </script>
 
-<button>Cart</button>
+<button class="button" on:click={toggle}>Cart</button>
 
 <!-- Add collapsible to render the products in the cart -->
+<div
+  class="mini-cart"
+  style:display={$visible ? null : 'none'}
+  use:clickOutside={() => ($visible = false)}
+>
+  {#if cart.items.length > 0}
+    <ul>
+      {#each cart.items as item}
+        {@const product = item.product}
+        <li class="product-item">
+          <img class="product-item-photo" src={product.image.url} alt="" />
+          <div class="product-item-details">
+            <p class="product-item-name">{product.name}</p>
+            <div class="product-item-price">
+              <PriceBox priceRange={product.price_range} />
+            </div>
+            <div class="product-item-quantity">
+              Quantity: {item.quantity}
+            </div>
+          </div>
+        </li>
+      {/each}
+    </ul>
+  {:else}
+    <p>Cart is empty</p>
+  {/if}
+</div>
+
+<style>
+  .mini-cart {
+    position: absolute;
+    top: 10rem;
+    right: 0;
+    border: 1px solid black;
+    background: white;
+    padding: 1rem;
+    width: 300px;
+  }
+
+  .mini-cart ul {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+  }
+
+  .product-item {
+    display: flex;
+  }
+
+  .product-item-photo {
+    width: 100px;
+    height: 100px;
+  }
+</style>
