c1dea0d45d7673ec3807dd0d26243f7ba7263883
ubermanu <manu@paca.happy-dev.fr>
2023-11-27 19:12:19 +0100

    feat: breadcrumbs

---
+++ src/lib/components/Breadcrumbs.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  interface BreadcrumbItem {
+    text: string
+    href?: string
+  }
+
+  export let items: BreadcrumbItem[] = []
+</script>
+
+{#if items.length > 0}
+  <ul class="breadcrumbs">
+    {#each items as item, i}
+      {#if i > 0}
+        <span class="separator">/</span>
+      {/if}
+      {#if item.href && i < items.length - 1}
+        <a href={item.href}>{item.text}</a>
+      {:else}
+        <span>{item.text}</span>
+      {/if}
+    {/each}
+  </ul>
+{/if}
+
+<style>
+  .breadcrumbs {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 0.4rem;
+    margin: 0;
+    padding: 0;
+    font-size: 0.8rem;
+    list-style: none;
+  }
+</style>
--- src/routes/(catalog)/products/[sku]/+page.svelte
+++ src/routes/(catalog)/products/[sku]/+page.svelte
@@ -1,12 +1,18 @@
 <script>
+  import Breadcrumbs from '$lib/components/Breadcrumbs.svelte'
   import PriceBox from '$lib/components/PriceBox.svelte'
 
   export let data
+
+  // TODO: Add category to breadcrumbs
+  const breadcrumbs = [{ text: 'Home', href: '/' }, { text: data.product.name }]
 </script>
 
 <svelte:head>
   <title>{data.product.name}</title>
 </svelte:head>
+
+<Breadcrumbs items={breadcrumbs} />
 
 <h1>{data.product.name}</h1>
 <p>{@html data.product.description.html}</p>
