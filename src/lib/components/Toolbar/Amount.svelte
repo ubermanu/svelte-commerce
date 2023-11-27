e5be880a569838f75a3660a9942727270a5c92ac
ubermanu <manu@paca.happy-dev.fr>
2023-11-28 00:34:05 +0100

    feat: toolbar amount

---
+++ src/lib/components/Toolbar/Amount.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  export let pageSize: number
+  export let currentPage: number
+  export let totalCount: number
+</script>
+
+{#if totalCount > 0}
+  <div class="toolbar-amount">
+    {(currentPage - 1) * pageSize + 1} - {Math.min(
+      currentPage * pageSize,
+      totalCount
+    )} of {totalCount}
+  </div>
+{/if}
