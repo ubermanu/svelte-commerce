56b8e9f60b9e7e23a3a125caf5c0de8dd9d78cee
ubermanu <manu@paca.happy-dev.fr>
2023-11-29 00:24:25 +0100

    update create account form styles

--- src/routes/(store)/customer/account/create/+page.svelte
+++ src/routes/(store)/customer/account/create/+page.svelte
@@ -1,6 +1,12 @@
 <script>
+  import Input from '$lib/components/Form/Input.svelte'
+  import ActionsToolbar from '$lib/components/ActionsToolbar.svelte'
+  import Checkbox from '$lib/components/Form/Checkbox.svelte'
+
   export let data
   export let form
+
+  let showPassword = false
 </script>
 
 <svelte:head>
@@ -19,13 +25,51 @@
   <p>Account created successfully</p>
 {/if}
 
-<form action="?/create" method="post">
-  <fieldset>
-    <legend>Sign Up</legend>
-    <input type="text" name="firstname" placeholder="First Name" />
-    <input type="text" name="lastname" placeholder="Last Name" />
-    <input type="email" name="email" placeholder="Email" />
-    <input type="password" name="password" placeholder="Password" />
-    <input type="submit" value="Login" />
+<form
+  class="form create account form-create-account"
+  action="?/create"
+  method="post"
+>
+  <fieldset class="fieldset create info">
+    <legend>Personal Information</legend>
+    <Input label="First Name" name="firstname" required />
+    <Input label="Last Name" name="lastname" required />
+    <Checkbox label="Sign Up for Newsletter" name="is_subscribed" />
+    <Checkbox
+      label="Allow remote shopping assistance"
+      name="assistance_allowed"
+    />
   </fieldset>
+  <fieldset class="fieldset create account">
+    <legend>Sign-in Information</legend>
+    <Input label="Email" name="email" type="email" required />
+    <Input
+      label="Password"
+      name="password"
+      type={showPassword ? 'text' : 'password'}
+      required
+    />
+    <Input
+      label="Confirm Password"
+      name="password_confirmation"
+      type={showPassword ? 'text' : 'password'}
+      required
+    />
+    <Checkbox
+      label="Show Password"
+      bind:value={showPassword}
+      on:change={() => (showPassword = !showPassword)}
+    />
+  </fieldset>
+  <ActionsToolbar>
+    <button slot="primary" type="submit" class="action submit primary">
+      Create an Account
+    </button>
+  </ActionsToolbar>
 </form>
+
+<style lang="postcss">
+  .fieldset {
+    @apply mb-6;
+  }
+</style>
