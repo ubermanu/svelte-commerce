03448ea2ddeaba64afa3c77e5c6c1832cc8e744f
ubermanu <manu@paca.happy-dev.fr>
2023-11-28 23:00:13 +0100

    feat: typography + forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
+import forms from '@tailwindcss/forms'
+import typography from '@tailwindcss/typography'
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [typography, forms],
 }
