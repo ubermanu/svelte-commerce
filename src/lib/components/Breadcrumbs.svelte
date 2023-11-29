c5360052c0c26d9fab29ff85d849e5fa2b689441
ubermanu <manu@paca.happy-dev.fr>
2023-11-29 16:13:22 +0100

    export separator character for breadcrumbs

--- src/lib/components/Breadcrumbs.svelte
+++ src/lib/components/Breadcrumbs.svelte
@@ -5,33 +5,23 @@
   }
 
   export let items: BreadcrumbItem[] = []
+
+  export let separator: string = '/'
 </script>
 
 {#if items.length > 0}
-  <ul class="breadcrumbs">
+  <ul class="breadcrumbs flex flex-row items-center gap-2 text-sm">
     {#each items as item, i}
       {#if i > 0}
-        <span class="separator">/</span>
+        <span class="separator">{separator}</span>
       {/if}
       {#if item.href && i < items.length - 1}
-        <a class="item" href={item.href}>{item.text}</a>
+        <a class="underline-offset-4 hover:underline" href={item.href}>
+          {item.text}
+        </a>
       {:else}
-        <span class="item">{item.text}</span>
+        <span class="text-neutral-500">{item.text}</span>
       {/if}
     {/each}
   </ul>
 {/if}
-
-<style lang="postcss">
-  .breadcrumbs {
-    @apply flex flex-row items-center gap-2 text-sm;
-  }
-
-  a.item {
-    @apply underline-offset-4 hover:underline;
-  }
-
-  span.item {
-    @apply text-neutral-500;
-  }
-</style>
