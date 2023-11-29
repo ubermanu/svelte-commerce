4d2ae442829a5ad892db32189612e5f1f1ac509b
ubermanu <manu@paca.happy-dev.fr>
2023-11-29 12:44:44 +0100

    close button in minicart

--- src/lib/components/MiniCart.svelte
+++ src/lib/components/MiniCart.svelte
@@ -1,22 +1,36 @@
 <script lang="ts">
   import PriceBox from '$lib/components/PriceBox.svelte'
-  import { ShoppingCart } from 'svelte-lucide'
+  import { ShoppingCart, X } from 'svelte-lucide'
   import { createPopover, melt } from '@melt-ui/svelte'
 
   export let cart: any
 
   const {
-    elements: { trigger, content },
-  } = createPopover()
+    elements: { trigger, content, close },
+  } = createPopover({
+    positioning: {
+      placement: 'top-end',
+    },
+  })
 </script>
 
 <button use:melt={$trigger} class="mini-cart-button p-2" title="My Cart">
   <ShoppingCart tabindex="-1" aria-hidden="true" />
 </button>
 
-<div use:melt={$content} class="mini-cart">
+<div
+  use:melt={$content}
+  class="mini-cart w-full max-w-lg bg-white p-4 pt-8 shadow-lg"
+>
+  <button
+    use:melt={$close}
+    class="close-button absolute right-1 top-2"
+    title="Close"
+  >
+    <X tabindex="-1" aria-hidden="true" />
+  </button>
   {#if cart.items.length > 0}
-    <ul>
+    <ul class="items space-y-2">
       {#each cart.items as item}
         {@const product = item.product}
         <li class="product-item">
@@ -36,32 +50,20 @@
         </li>
       {/each}
     </ul>
-    <a href="/checkout/cart">View cart</a>
+    <a href="/checkout/cart" class="action primary checkout mt-4 w-full">
+      View cart
+    </a>
   {:else}
     <p>Cart is empty</p>
   {/if}
 </div>
 
-<style>
-  .mini-cart {
-    border: 1px solid black;
-    background: white;
-    padding: 1rem;
-    width: 300px;
-  }
-
-  .mini-cart ul {
-    margin: 0;
-    padding: 0;
-    list-style: none;
-  }
-
+<style lang="postcss">
   .product-item {
-    display: flex;
+    @apply flex items-center gap-4;
   }
 
   .product-item-photo {
-    width: 100px;
-    height: 100px;
+    @apply h-24 w-24 object-contain;
   }
 </style>
