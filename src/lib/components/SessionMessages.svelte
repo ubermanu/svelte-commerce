aab4a333e67cd0718f911feeb25b20c9913b9eb9
ubermanu <manu@paca.happy-dev.fr>
2023-12-02 19:03:07 +0100

    hide session messages when navigating on another page

--- src/lib/components/SessionMessages.svelte
+++ src/lib/components/SessionMessages.svelte
@@ -1,11 +1,19 @@
 <script>
   import { page } from '$app/stores'
+  import { onNavigate } from '$app/navigation'
   import Message from '$lib/components/Message.svelte'
+
+  let messages = $page.data.messages
+
+  // Clear messages when navigating away from the page
+  onNavigate(() => {
+    messages = []
+  })
 </script>
 
-{#if $page.data?.messages.length > 0}
+{#if messages.length > 0}
   <div class="messages space-y-4">
-    {#each $page.data.messages as message}
+    {#each messages as message}
       <Message level={message.type}>{message.body}</Message>
     {/each}
   </div>
