7e8b0ce18f227dff29cede1e97128a92ce8ebeb6
ubermanu <manu@paca.happy-dev.fr>
2024-02-04 23:40:58 +0100

    update product list styles

--- src/lib/components/ProductList.svelte
+++ src/lib/components/ProductList.svelte
@@ -6,22 +6,27 @@
   export let mode: 'grid' | 'list' = 'grid'
 </script>
 
-<ul
-  class="items"
+<ol
+  class="products items"
   class:products-grid={mode === 'grid'}
   class:products-list={mode === 'list'}
 >
   {#each products as product}
     <li class="product-item">
       <a href="/products/{product.sku}" tabindex="-1">
-        <img class="product-item-photo" src={product.image?.url} alt="" />
+        <img
+          loading="lazy"
+          class="product-item-photo"
+          src={product.image?.url}
+          alt=""
+        />
       </a>
       <a href="/products/{product.sku}" class="product-item-link">
         <h3 class="product-item-name">{product.name}</h3>
       </a>
     </li>
   {/each}
-</ul>
+</ol>
 
 <style>
   .product-item {
@@ -30,6 +35,7 @@
   }
 
   .product-item-photo {
+    aspect-ratio: 1080 / 1340;
     width: 100%;
     max-width: 300px;
     height: auto;
