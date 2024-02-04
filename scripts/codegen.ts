6ee07aacb7f24979b49bba5ab1c18a42c94bbfcf
ubermanu <manu@paca.happy-dev.fr>
2024-02-04 15:20:41 +0100

    remove get schema unnecessary step

--- scripts/codegen.ts
+++ scripts/codegen.ts
@@ -1,12 +1,17 @@
 import type { CodegenConfig } from '@graphql-codegen/cli'
+import process from 'node:process'
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: './schema.graphql',
+  schema: process.env.PRIVATE_MAGENTO_BASE_URL + '/graphql',
+  documents: ['src/**/*.graphql'],
   generates: {
-    'src/lib/generated/graphql.types.ts': {
-      schema: './schema.graphql',
-      plugins: ['typescript'],
+    'src/lib/generated/graphql.ts': {
+      plugins: [
+        'typescript',
+        'typescript-operations',
+        'typescript-graphql-request',
+      ],
     },
   },
 }
